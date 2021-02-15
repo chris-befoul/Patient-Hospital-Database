bba310b0072204683729350a59d4c7d71b28d775
kimte124 <kimte@oregonstate.edu>
2021-02-15 15:49:05 -0500

    Added physiciansserverside.js, physicians.js, and dbcon.js

--- project/physiciansserverside.js
+++ project/physiciansserverside.js
@@ -2,25 +2,25 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-app.get('/', function(req, res, next){
-    console.log("test1")
-    res.render('physicians');
-    });
+    router.get('/', function(req, res, next){
+        console.log("test1")
+        res.render('physicians');
+        });
 
-// POST route to insert new entry into table.
-app.post('/', function(req,res,next){
-    var context = {};
-    console.log("test2")
-    pool.query("INSERT INTO Physicians (`lastName`, `firstName`, `specialty`) VALUES (?, ?, ?)", 
-    [req.query.lastName, req.query.firstName, req.query.specialty], function(err, result) {
-    if(err){
-        next(err);
-        return;
-    }
-    context.results = "Inserted id " + result.insertId;
-    res.render('physicians', context);
-    })
-});
+    // POST route to insert new entry into table.
+    router.post('/', function(req,res,next){
+        var context = {};
+        console.log("test2")
+        pool.query("INSERT INTO Physicians (`lastName`, `firstName`, `specialty`) VALUES (?, ?, ?)", 
+        [req.query.lastName, req.query.firstName, req.query.specialty], function(err, result) {
+        if(err){
+            next(err);
+            return;
+        }
+        context.results = "Inserted id " + result.insertId;
+        res.render('physicians', context);
+        })
+    });
 
     return router;
 }();
