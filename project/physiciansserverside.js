1e2719b6d78b89fcf96a38a2d65852f2a013d1ed
kimte124 <kimte@oregonstate.edu>
2021-02-15 14:38:19 -0500

    Added physiciansserverside.js, physicians.js, and dbcon.js

--- project/physiciansserverside.js
+++ project/physiciansserverside.js
@@ -1,22 +1,35 @@
-module.exports = function(){
-    var express = require('express');
-    var router = express.Router();
+// Snippets of code used from course modules, W3 Schools and GeeksforGeeks.
+// Import express and handlebars.
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var app = express();
+var mysql = require('./dbcon.js');
 
-    router.get('/', function(req, res, next){
-        res.render('physicians');
-      });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-    // POST route to insert new entry into table.
-    router.post('/', function(req,res,next){
-        var context = {};
-    
-        pool.query("INSERT INTO Physicians (`lastName`, `firstName`, `specialty`) VALUES (?, ?, ?)", 
-        [req.query.lastName, req.query.firstName, req.query.specialty], function(err, result) {
-        if(err){
-            next(err);
-            return;
-        }
-        context.results = "Inserted id " + result.insertId;
-        res.render('physicians', context);
-        })
-    })};
+var bodyParser = require('body-parser');
+
+// Tell node to choose parser depending on whether it see URL or JSON.
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+router.get('/', function(req, res, next){
+    res.render('physicians');
+    });
+
+// POST route to insert new entry into table.
+router.post('/', function(req,res,next){
+    var context = {};
+
+    pool.query("INSERT INTO Physicians (`lastName`, `firstName`, `specialty`) VALUES (?, ?, ?)", 
+    [req.query.lastName, req.query.firstName, req.query.specialty], function(err, result) {
+    if(err){
+        next(err);
+        return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    res.render('physicians', context);
+    })
+});
