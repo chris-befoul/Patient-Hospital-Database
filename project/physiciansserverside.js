9e4cf2636f174353842aa015a99c239f5985b6df
kimte124 <kimte@oregonstate.edu>
2021-02-15 15:52:15 -0500

    Added physiciansserverside.js, physicians.js, and dbcon.js

--- project/physiciansserverside.js
+++ project/physiciansserverside.js
@@ -2,7 +2,7 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    router.get('/', function(req, res, next){
+    router.get('/physicians', function(req, res, next){
         console.log("test1")
         res.render('physicians');
         });
