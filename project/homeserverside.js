5c18bbe9dc7644364720f72361f431f35ddd26c4
kimte124 <kimte@oregonstate.edu>
2021-02-15 16:14:08 -0500

    Added physiciansserverside.js, physicians.js, and dbcon.js

--- project/homeserverside.js
+++ project/homeserverside.js
@@ -4,6 +4,7 @@ var express = require('express');
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 var app = express();
 var mysql = require('./dbcon.js');
+var cors = require('cors')
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -15,27 +16,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('port', process.argv[2]);
-
-// Add headers
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + app.get('port'));
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+app.use(cors())
+ 
 app.get('/', function(req, res, next){
   res.render('home');
 });
