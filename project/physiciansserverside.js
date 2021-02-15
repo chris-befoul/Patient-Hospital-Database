5878e7a57da39d8bfdae91f8f1ab817d591c7360
kimte124 <kimte@oregonstate.edu>
2021-02-15 15:48:02 -0500

    Added physiciansserverside.js, physicians.js, and dbcon.js

--- project/physiciansserverside.js
+++ project/physiciansserverside.js
@@ -1,19 +1,6 @@
-// Snippets of code used from course modules, W3 Schools and GeeksforGeeks.
-// Import express and handlebars.
-var express = require('express');
-var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-var app = express();
-var mysql = require('./dbcon.js');
-
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-var bodyParser = require('body-parser');
-
-// Tell node to choose parser depending on whether it see URL or JSON.
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(express.static('public'));
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
 
 app.get('/', function(req, res, next){
     console.log("test1")
@@ -34,3 +21,6 @@ app.post('/', function(req,res,next){
     res.render('physicians', context);
     })
 });
+
+    return router;
+}();
