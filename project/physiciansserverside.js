ffd0063f2322a350fb30271f21a3683a6573f0da
kimte124 <kimte@oregonstate.edu>
2021-02-16 12:44:15 -0500

    Added physiciansserverside.js, physicians.js, and dbcon.js

--- project/physiciansserverside.js
+++ project/physiciansserverside.js
@@ -8,7 +8,7 @@ module.exports = function(){
       var mysql = req.app.get('mysql');
         
       // Query to return everything from the table and format the date as month-day-year.
-      mysql.pool.query('SELECT id, lastName, firstName, specialty FROM Physicians', function(err, rows, fields){
+      mysql.pool.query('SELECT physicianID, lastName, firstName, specialty FROM Physicians', function(err, rows, fields){
         if(err){
           next(err);
           return;
